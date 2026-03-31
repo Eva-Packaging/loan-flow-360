8710ca706fdf43eb9a1c234c164292f32c3c3779
Kirubel Fanta <kirubelfanta12@gmail.com>
2026-03-31 17:07:58 -0400

    feat(SCRUM-541,542,543): initialise underwriting service with actuator and profile configuration

---
+++ backend/underwriting-service/underwriting-service/src/test/java/com/loanflow/underwriting_service/UnderwritingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loanflow.underwriting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderwritingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
