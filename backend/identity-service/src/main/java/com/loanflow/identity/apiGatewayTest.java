57f8274b20eff073828dc3f4e9e48a877ae8441c
nouha belkacemi <nouhabelka2001@gmail.com>
2026-04-07 12:00:37 -0400

    LF3-154 gateway api routing to two diffrent routes

--- backend/identity-service/src/main/java/com/loanflow/identity/apiGatewayTest.java
+++ backend/identity-service/src/main/java/com/loanflow/identity/apiGatewayTest.java
@@ -1,10 +1,12 @@
 package com.loanflow.identity;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/identityService")
 public class apiGatewayTest {
     @GetMapping("/apiGatewayTest")
     public String getApiGatewayTest() {
