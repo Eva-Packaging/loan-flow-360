8275f3aeea89bc37c9fdcc77ebb8a33624be628d
badreddin98 <elyazgi98@gmail.com>
2026-04-13 23:46:42 -0400

    LF3-95 Create ManualReviewNote entity and repository

--- backend/underwriting-service/src/main/java/com/loanflow/underwritingservice/repository/UnderwritingCheckRepository.java
+++ backend/underwriting-service/src/main/java/com/loanflow/underwritingservice/repository/UnderwritingCheckRepository.java
@@ -3,10 +3,12 @@ package com.loanflow.underwritingservice.repository;
 import com.loanflow.underwritingservice.entities.UnderwritingChecks;
 import com.loanflow.underwritingservice.entities.enums.CheckType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UnderwritingCheckRepository extends JpaRepository<UnderwritingChecks, Long> {
 
     List<UnderwritingChecks> findByUnderwritingCase_underWritingCaseId(Long caseId);
