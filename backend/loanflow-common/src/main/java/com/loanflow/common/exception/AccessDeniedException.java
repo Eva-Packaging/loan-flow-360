519086dfe68baafcb4ba8453f2b9fc67122f578d
NegaNote <neganote43@gmail.com>
2026-09-02 15:42:30 -0400

    LF3-20 Create missing domain-specific exceptions (rename AccessDeniedException to ForbiddenException)

--- backend/loanflow-common/src/main/java/com/loanflow/common/exception/AccessDeniedException.java
+++
@@ -1,7 +0,0 @@
-package com.loanflow.common.exception;
-
-public class AccessDeniedException extends LoanFlowException {
-    public AccessDeniedException(String message) {
-        super(message, ErrorCode.FORBIDDEN);
-    }
-}
