1a30b0b0e4bb65b72662b4d044590bf8f4ce0ffc
NegaNote <neganote43@gmail.com>
2026-09-02 16:06:41 -0400

    LF3-21 Create GlobalExceptionHandler (and update ErrorCode to use HttpStatus)

--- backend/loanflow-common/src/main/java/com/loanflow/common/exception/ErrorCode.java
+++ backend/loanflow-common/src/main/java/com/loanflow/common/exception/ErrorCode.java
@@ -1,21 +1,22 @@
 package com.loanflow.common.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
-    RESOURCE_NOT_FOUND(404),
-    DUPLICATE_RESOURCE(409),
-    VALIDATION_FAILED(400),
-    UNAUTHORIZED(401),
-    FORBIDDEN(403),
-    SERVICE_UNAVAILABLE(503),
-    INTERNAL_ERROR(500),
-    EVENT_PROCESSING_FAILED(500);
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND),
+    DUPLICATE_RESOURCE(HttpStatus.CONFLICT),
+    VALIDATION_FAILED(HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED),
+    FORBIDDEN(HttpStatus.FORBIDDEN),
+    SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE),
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR),
+    EVENT_PROCESSING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR);
 
     @Getter
-    private final int statusCode;
+    private final HttpStatus httpStatus;
 
-    ErrorCode(int statusCode) {
-        this.statusCode = statusCode;
+    ErrorCode(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
     }
 }
