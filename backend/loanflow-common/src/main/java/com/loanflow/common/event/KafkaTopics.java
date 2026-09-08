9faa6663b1f12a2dc776b790dc346996cd1c1d03
NegaNote <neganote43@gmail.com>
2026-09-08 11:24:49 -0400

    LF3-27 Create KafkaTopics constants class

---
+++ backend/loanflow-common/src/main/java/com/loanflow/common/event/KafkaTopics.java
@@ -0,0 +1,12 @@
+package com.loanflow.common.event;
+
+public final class KafkaTopics {
+    private KafkaTopics() {}
+
+    public static final String LOAN_APPLICATION_SUBMITTED = "loan-application-submitted";
+    public static final String LOAN_DOCUMENT_VERIFIED = "loan-document-verified";
+    public static final String LOAN_UNDERWRITING_REQUESTED = "loan-underwriting-requested";
+    public static final String LOAN_UNDERWRITING_COMPLETED = "loan-underwriting-completed";
+    public static final String LOAN_STATUS_UPDATED = "loan-status-updated";
+    public static final String LOAN_NOTIFICATIONS_SEND = "loan-notifications-send";
+}
